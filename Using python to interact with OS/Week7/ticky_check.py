8bc89c98b25cb01f1f0ab1580c82d5d737c30b32
CaDucT <nurik39@gmail.com>
2022-06-06 12:32:13 +0200

    update

--- Using python to interact with OS/Week7/ticky_check.py
+++ Using python to interact with OS/Week7/ticky_check.py
@@ -1,63 +1,55 @@
-#!/usr/bin/env python3
-
 import re
 import operator
 import csv
 
-error = {}
 per_user = {}
-user_count = []
+error = {}
+
+
 
 def open_files(file):
     with open(file, 'r') as f:
-        data = f.readlines()
-        return data
-
-def generate_dict(data):
-    for line in data:
-        username = re.search(r'\(([\w\. ]*)\)', line)
-        errorname = re.search(r'ticky: ERROR ([\w ]*)', line)
-
-        if errorname != None:
-            if errorname[1] not in error:
-                error[errorname[1]] = 1
-            else:
-                error[errorname[1]] += 1
-        elif username != None:
-            if username[1] not in per_user:
-                per_user[username[1]] = {}
-                per_user[username[1]]['INFO'] = 0
-                per_user[username[1]]['ERROR'] = 0
-                if errorname != None:
-                    per_user[username[1]]['ERROR'] = 1
+        for line in f:
+            errorname = re.search(r'ERROR ([\w \']*)', line)
+            if errorname != None:
+                if errorname[1] not in error:
+                    error[errorname[1]] = 1
                 else:
-                    per_user[username[1]]['INFO'] = 1
-            else:
-                if errorname != None:
-                    per_user[username[1]]['ERROR'] += 1
-                else:
-                    per_user[username[1]]['INFO'] += 1
-    errors_list = sorted(error.items(), key=operator.itemgetter(1), reverse=True)
-    errors_list.insert(0, ('Error', 'Count'))
-    per_user_list = sorted(per_user.items(), key=operator.itemgetter(0))
-    per_user_list.insert(0, ('Username', {'INFO', 'ERROR'}))
+                    error[errorname[1]] += 1
+        sorted_errors = sorted(error.items(), key=operator.itemgetter(1), reverse=True)
+        print(sorted_errors)
 
-    with open('error_messages.csv', 'w') as f:
-        writer = csv.writer(f)
-        for key, value in errors_list:
-            writer.writerow([key, vale])
+    with open('error_messages.csv', 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',')
+        spamwriter.writerow(["Error", "Count"])
+        for log in sorted_errors:
+            key, value = log
+            spamwriter = csv.writer(csvfile, delimiter=',')
+            spamwriter.writerow([key, value])
 
-    with open('user_statistics.csv', 'w') as f2:
-        writer = csv.writer(f2)
-        for i, key in per_user_list:
-            if i != 'Username':
-                for j, x in key.items():
-                    if len(user_count) == 2:
-                        user_count.clear()
-                    user_count.append(x)
-                writer.writerow([i, user_count[0], user_count[1]])
+    with open(file, 'r') as f:
+        for line in f:
+            usr_pat = re.search(r'\(([\w\.]*)\)', line)
+            err_pat = re.search(r'(ERROR|INFO)', line)
+            if usr_pat[1] not in per_user.keys():
+                per_user[usr_pat[1]] = {}
+                per_user[usr_pat[1]]['INFO'] = 0
+                per_user[usr_pat[1]]['ERROR'] = 0
 
-print(open_files('syslog.log'))
+            if err_pat[1] == 'INFO':
+                per_user[usr_pat[1]]['INFO'] += 1
+            elif err_pat[1] == 'ERROR':
+                per_user[usr_pat[1]]['ERROR'] += 1
+        sorted_users = sorted(per_user.items())
+        print(sorted_users)
+
+    f = open('user_statistics.csv', 'w', newline='')
+    write = csv.writer(f, delimiter=',')
+    write.writerow(['Username', 'INFO', 'ERROR'])
+    for stats in sorted_users:
+        a, b = stats
+        f.write(str(a) + ',' + str(b["INFO"]) + ',' + str(b["ERROR"]) + '\n')
+    f.close()
 
 
 
@@ -66,4 +58,13 @@ print(open_files('syslog.log'))
 
 
 
-#Jan 31 00:09:39 ubuntu.local ticky: INFO Created ticket [#4217] (mdouglas)
+
+
+
+
+
+
+
+open_files('syslog.log')
+
+
